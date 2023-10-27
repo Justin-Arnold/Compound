7e1059a9d904af1d360b1958d75c206f0528a039
Justin Arnold <justin.arnold@programmer.net>
2023-10-27 19:54:32 -0400

    scaffold out task view

--- frontend/pages/app.vue
+++ frontend/pages/app.vue
@@ -14,9 +14,14 @@ function signOut() {
 </script>
 
 <template>
-    <div>
-        I am an "App" <button @click="signOut()">Sign Out</button>
-        <br/>
+    <div class="h-screen flex flex-col bg-slate-800 text-white">
+        <div class="bg-slate-200 w-full flex justify-between items-center p-2 text-black">
+            Compound
+            <div class="flex gap-2">
+                <NuxtLink to="/app/task" class="p-2 bg-slate-400 rounded">View Tasks</NuxtLink>
+                <button @click="signOut()" class="p-2 bg-slate-400 rounded">Sign Out</button>
+            </div>
+        </div>
         <NuxtPage/>
     </div>
 </template>
