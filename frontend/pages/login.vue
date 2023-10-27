47a1fbf20bca54e3f0511f327c01d034ccec90a1
Justin Arnold <justin.arnold@programmer.net>
2023-10-27 19:50:56 -0400

    scaffold login page

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -8,11 +8,16 @@ const signInWithOAuth = async () => {
         password: 'password',
     })
     if (error) console.log(error)
+    else navigateTo('/')
 }
 
 </script>
 
 <template>
-    You need to login buster
-    <button @click="signInWithOAuth()">Login</button>
+    <div class="h-screen bg-slate-800 text-white grid place-items-center">
+        <div class="h-40 p-4 flex flex-col gap-4">
+            <div class="text-2xl">You need to login buster</div>
+            <button @click="signInWithOAuth()" class="p-2 bg-slate-200 text-black rounded">Login</button>
+        </div>
+    </div>
 </template>
