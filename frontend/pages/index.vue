37c9a1cad98cbe881b6ef931663fee08f41e8a3d
Justin Arnold <justin.arnold@programmer.net>
2023-10-27 19:42:24 -0400

    add tailwind

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,4 +1,6 @@
 <template>
-    Hello World
-    <NuxtLink to="/app">App</NuxtLink>
+    <div class="bg-slate-200 w-full flex justify-between items-center p-2">
+        Compound
+        <NuxtLink to="/app" class="p-2 bg-slate-400 rounded">Go To App</NuxtLink>
+    </div>
 </template>
