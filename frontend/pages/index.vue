b4c2868e5e5e008ba2fbea59f4b5811b3f5ca1c6
Justin Arnold <justin.arnold@programmer.net>
2023-10-27 19:48:09 -0400

    finish main page scaffold

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,14 @@
 <template>
-    <div class="bg-slate-200 w-full flex justify-between items-center p-2">
-        Compound
-        <NuxtLink to="/app" class="p-2 bg-slate-400 rounded">Go To App</NuxtLink>
+    <div class="h-screen bg-slate-800 text-white">
+        <div class="bg-slate-200 w-full flex justify-between items-center p-2 text-black">
+            Compound
+            <NuxtLink to="/app" class="p-2 bg-slate-400 rounded">Go To App</NuxtLink>
+        </div>
+        <div class="grid place-items-center text-lg font-bold h-full p-2">
+            <div class="flex flex-col gap-8 items-center">
+                <span class="text-[100px]">👷</span>
+                <p>Do not forget to wear hardhats while the site is under construction. </p>
+            </div>
+        </div>
     </div>
 </template>
