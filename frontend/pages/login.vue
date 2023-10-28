7bb8f1593a75117c764973357056259a8cba7ae2
Justin Arnold <justin.arnold@programmer.net>
2023-10-28 12:38:19 -0400

    create real login flow

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -2,12 +2,12 @@
 
 const supabase = useSupabaseClient()
 
-const signInWithOAuth = async () => {
-    const { error } = await supabase.auth.signInWithPassword({
-        email: 'random@test.com',
-        password: 'password',
-    })
-    if (error) console.log(error)
+async function signIn(event: any) {
+    const email = event.target.email.value
+    const password = event.target.password.value
+
+    const { error } = await supabase.auth.signInWithPassword({email,password,})
+    if (error) alert(error)
     else navigateTo('/')
 }
 
@@ -17,7 +17,11 @@ const signInWithOAuth = async () => {
     <div class="h-screen bg-slate-800 text-white grid place-items-center">
         <div class="h-40 p-4 flex flex-col gap-4">
             <div class="text-2xl">You need to login buster</div>
-            <button @click="signInWithOAuth()" class="p-2 bg-slate-200 text-black rounded">Login</button>
+            <form @submit.prevent="signIn" class="flex flex-col gap-2">
+                <input name="email" type="text" placeholder="email" class="p-2 rounded text-black"/>
+                <input name="password" type="password" placeholder="password" class="p-2 rounded text-black"/>
+                <button type="submit" class="p-2 rounded bg-slate-200 text-black">Sign In</button>
+            </form>
         </div>
     </div>
 </template>
