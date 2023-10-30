d56c840adb382124d5802d4d9930e139ff02ec49
Justin Arnold <justin.arnold@programmer.net>
2023-10-30 15:26:00 -0400

    allow creating points

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -3,3 +3,10 @@
     <NuxtPage />
   </div>
 </template>
+
+<style>
+    ::backdrop {
+        background-color: rgba(0, 0, 0, 0.2);
+        backdrop-filter: blur(4px);
+    }
+</style>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -27,14 +27,37 @@ function signOut() {
     })
 }
 
+const newPointDialog = ref<HTMLDialogElement>()
+
 async function newPoint() {
+    console.log('new point', newPointDialog)
+    newPointDialog.value?.showModal()
+    // const { data, error } = await supabase.from('points').insert({
+    //     name: 'New Point',
+    //     user_id: user.value?.id || ''
+    // })
+    // if (error) console.log('e', error)
+    // else {
+    //     console.log('d', data)
+    // }
+}
+
+const name = ref<string>('')
+const type = ref<string>('')
+const frequency = ref<string>('')
+
+async function createPoint() {
     const { data, error } = await supabase.from('points').insert({
-        name: 'New Point',
+        name: name.value,
+        type: type.value,
+        frequency: frequency.value,
         user_id: user.value?.id || ''
     })
     if (error) console.log('e', error)
     else {
         console.log('d', data)
+        newPointDialog.value?.close()
+        state.todaysPoints.push(data)
     }
 }
 </script>
@@ -72,5 +95,34 @@ async function newPoint() {
                 </div>
             </div>
         </div>
+        <dialog id="dia" ref="newPointDialog">
+            <div class="bg-slate-800 rounded-lg p-4 flex flex-col gap-4 w-[400px]">
+                <h2 class="text-2xl font-semibold text-purple-100">New Point</h2>
+                <div class="flex flex-col gap-2">
+                    <label class="text-slate-100">Name</label>
+                    <input class="rounded p-2 bg-slate-700 text-slate-100" type="text" placeholder="Name" v-model="name"/>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <label class="text-slate-100">Type</label>
+                    <select class="rounded p-2 bg-slate-700 text-slate-100" v-model="type">
+                        <option value="Binary">Binary</option>
+                        <option value="Tally">Tally</option>
+                    </select>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <label class="text-slate-100">Frequency</label>
+                    <select class="rounded p-2 bg-slate-700 text-slate-100" v-model="frequency">
+                        <option value="day">Per Day</option>
+                        <option value="week">Per Week</option>
+                        <option value="month">Per Month</option>
+                         <option value="year">Per Year</option>
+                    </select>
+                </div>
+                <div class="flex gap-2">
+                    <button class="rounded p-2 bg-slate-500/50 text-slate-400" @click="createPoint">Create</button>
+                    <button class="rounded p-2 bg-slate-500/50 text-slate-400" @click="newPointDialog?.close()">Cancel</button>
+                </div>
+            </div>
+        </dialog>
     </div>
 </template>
