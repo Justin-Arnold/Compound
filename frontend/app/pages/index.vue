12c4f24c6a34861adcd152159d2574e42472fd1a
Justin Arnold <justin.arnold@programmer.net>
2023-10-29 20:05:56 -0400

    update app ui to move towards mvp

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
+import type { Database } from 'database';
 
 definePageMeta({
      middleware: 'auth'
 })
-const supabase = useSupabaseClient()
+
+const supabase = useSupabaseClient<Database>()
+const user = useSupabaseUser()
+
+const { data: points, error } = await supabase
+    .from('habits')
+    .select('name')
+
+const state = reactive<{
+    todaysPoints: any[]
+}>({
+    todaysPoints: points || []
+})
+
+
 
 function signOut() {
     supabase.auth.signOut()
@@ -11,21 +26,47 @@ function signOut() {
         navigateTo('/')
     })
 }
+
+async function newPoint() {
+    const { data, error } = await supabase.from('habits').insert({
+        name: 'New Point',
+        user_id: user.value?.id || ''
+    })
+    if (error) console.log('e', error)
+    else {
+        console.log('d', data)
+    }
+}
 </script>
 
 <template>
-    <div class="h-screen flex flex-col bg-slate-800 text-white">
+    <div class="h-screen flex flex-col bg-[#060a1d] text-white">
         <BaseHeader>
             <template #right>
                 <div class="flex gap-2">
-                    <NuxtLink to="/task" class="p-2 bg-slate-400 rounded">View Tasks</NuxtLink>
                     <button @click="signOut()" class="p-2 bg-slate-400 rounded">Sign Out</button>
                 </div>
             </template>
         </BaseHeader>
         <!-- Hero Section -->
         <div class="overflow-clip">
-            <div class="h-[400px] bg-gradient-to-br from-purple-900 to-blue-800 blur-4xl from-40% w-full"></div>
+            <div class="h-[1200px] bg-gradient-to-br from-[#06162b] to-[#2f879a] blur-4xl from-10% w-full"></div>
+        </div>
+        <div class="grid h-full grid-cols-3 p-4 gap-4">
+            <div class="bg-slate-700/50 rounded-lg aspect-square p-4 flex flex-col gap-4">
+                <h2 class="text-2xl font-semibold text-purple-100">Todays Points</h2>
+                <div v-if="state.todaysPoints.length > 0" class="flex flex-col gap-2">
+                    <div v-for="point, index in state.todaysPoints" :key="index" class="bg-slate-600 text-slate-100 rounded p-2">
+                        <p>{{ point.name }}</p>
+                    </div>
+                </div>
+                <div v-else class="grid h-1/2 place-items-center">
+                    <div class="flex flex-col items-center gap-4">
+                        <p class="text-3xl font-bold text-slate-500">No points today</p>
+                        <button class="rounded p-2 bg-slate-500/50 text-slate-400" @click="newPoint()">Create Point</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
--- frontend/base/components/BaseHeader.vue
+++ frontend/base/components/BaseHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-slate-800 h-16 p-4 flex justify-between items-center">
+    <div class="bg-[#060a1d] h-16 p-4 flex justify-between items-center">
         <NuxtLink class="text-white text-xl font-bold font-mono tracking-widest" :to="useRuntimeConfig().public.siteURL">
             [CmPd]
         </NuxtLink>
