cdad52a24cdb90901819e7c08ae373820af5773a
Justin Arnold <justin.arnold@programmer.net>
2023-11-01 21:10:31 -0400

    fix scoped view

--- frontend/app/pages/point-[pointId].vue
+++ frontend/app/pages/point-[pointId].vue
@@ -11,40 +11,51 @@ const route = useRoute()
 const pointId = route.params.pointId as string
 
 const { data: events, error } = await supabase
-    .from('points_data')
+    .from('point_event')
     .select()
     .eq('point_id', pointId)
     .order('recorded_at', { ascending: true })
 
-function getChartDataFromEvents() {
+type ChartDataset = {
+    label: string;
+    data: number[];
+    fill: boolean;
+    borderColor: string;
+    tension: number;
+    backgroundColor: string;
+};
 
-    const data = reactive({
-        labels: [],
-        datasets: [{
-            label: 'Smoking',
-            data: [],
-            fill: true,
-            borderColor: '#ff82ff',
-            tension: 0.4,
-            backgroundColor: 'rgba(167,80,167,0.2)'
-        }]
-    })
+const chartData = reactive<{
+    labels: string[];
+    datasets: ChartDataset[];
+}>({
+    labels: [],
+    datasets: [{
+        label: 'Smoking',
+        data: [],
+        fill: true,
+        borderColor: '#ff82ff',
+        tension: 0.4,
+        backgroundColor: 'rgba(167,80,167,0.2)'
+    }]
+});
 
-    if (!events) return {}
 
+if (events){
     events.forEach((event) => {
         console.log('e', event)
-        if (!data.labels.includes(formatDateToMMDD(event.recorded_at || ''))) {
-            data.labels.push(formatDateToMMDD(event.recorded_at || ''))
-            data.datasets[0].data.push(event.tally_value || 0)
+        if (!chartData.labels.includes(formatDateToMMDD(event.recorded_at || ''))) {
+            chartData.labels.push('')
+            chartData.datasets[0].data.push(event.value || 0)
         } else {
-            const index = data.labels.indexOf(formatDateToMMDD(event.recorded_at || ''))
-            data.datasets[0].data[index] += event.tally_value || 0
+            const index = chartData.labels.indexOf(formatDateToMMDD(event.recorded_at || ''))
+            chartData.datasets[0].data[index] += event.value || 0
         }
-    })
-
-    return data
+})
 }
+
+
+
 
 function formatDateToMMDD(dateTimeStr: string) {
     const date = new Date(dateTimeStr);
@@ -61,42 +72,35 @@ function signOut() {
     })
 }
 
-const setChartOptions = () => {
-    const documentStyle = getComputedStyle(document.documentElement);
-    const textColor = 'white';
-    const textColorSecondary = 'white';
-    const surfaceBorder = 'lightgray';
-
-    return {
-        maintainAspectRatio: false,
-        aspectRatio: 0.6,
-        plugins: {
-            legend: {
-                labels: {
-                    color: textColor
-                }
-            }
-        },
-        scales: {
-            x: {
-                ticks: {
-                    color: textColorSecondary
-                },
-                grid: {
-                    color: surfaceBorder
-                }
-            },
-            y: {
-                ticks: {
-                    color: textColorSecondary,
-                },
-                grid: {
-                    color: surfaceBorder
-                }
+const chartOptions = {
+    maintainAspectRatio: false,
+    aspectRatio: 0.6,
+    plugins: {
+        legend: {
+            labels: {
+                color: 'white'
             }
         }
-    };
-}
+    },
+    scales: {
+        x: {
+            ticks: {
+                color: 'white'
+            },
+            grid: {
+                color: 'lightgray'
+            }
+        },
+        y: {
+            ticks: {
+                color: 'white',
+            },
+            grid: {
+                color: 'lightgray'
+            }
+        }
+    }
+};
 </script>
 
 <template>
@@ -112,7 +116,7 @@ const setChartOptions = () => {
         <div class="overflow-clip relative">
             <div class="h-[1200px] bg-gradient-to-br from-indigo-950 to-indigo-200 blur-xl w-full"></div>
             <div class="absolute h-full w-full top-0 left-0 z-2 ">
-                <PrimeChart type="line" class="h-full w-full" :data="getChartDataFromEvents()" :options="setChartOptions()" />
+                <PrimeChart type="line" class="h-full w-full" :data="chartData" :options="chartOptions" />
             </div>
         </div>
         <div class="grid h-full grid-cols-3 p-4 gap-4">
