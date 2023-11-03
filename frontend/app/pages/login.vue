22e44955d99a9e367e38c23e8ed3dfb14f4015a1
Justin Arnold <justin.arnold@programmer.net>
2023-11-02 22:32:29 -0400

    fix text color

--- frontend/app/pages/login.vue
+++ frontend/app/pages/login.vue
@@ -19,8 +19,8 @@ async function signIn(event: any) {
         <div class="h-40 p-4 flex flex-col gap-4 max-w-[400px] pt-20">
             <div class="text-2xl">You need to login buster</div>
             <form @submit.prevent="signIn" class="flex flex-col gap-2">
-                <input name="email" type="text" placeholder="email" class="p-2 rounded text-black"/>
-                <input name="password" type="password" placeholder="password" class="p-2 rounded text-black"/>
+                <input name="email" type="text" placeholder="email" class="p-2 rounded "/>
+                <input name="password" type="password" placeholder="password" class="p-2 rounded "/>
                 <button type="submit" class="p-2 rounded bg-slate-200 text-black">Sign In</button>
             </form>
         </div>
