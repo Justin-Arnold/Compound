85fe48d327cc6b9b964016ce8bf09c247b3b4594
Justin Arnold <justin.arnold@programmer.net>
2023-11-02 22:44:35 -0400

    remove code that was premature and causing a bug with getting todays points

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -99,9 +99,7 @@ async function increaseValue(point) {
 }
 
 function toLocalISOString(date: Date) {
-  const offset = date.getTimezoneOffset();
-  const adjustedDate = new Date(date.getTime() - offset * 60 * 1000);
-  return adjustedDate.toISOString().split('T')[0];
+  return date.toISOString().split('T')[0];
 }
 </script>
 
